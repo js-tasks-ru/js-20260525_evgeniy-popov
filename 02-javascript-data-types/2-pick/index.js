e0e2a0a491a3c3df9ce9e4d8cf8d07a3af1416f9
eugenyly <eugenypop@gmail.com>
2026-06-03 21:44:15 +0200

    +upd: pick;

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const fieldsArr = Array.from(fields);
+  const result = {};
+  for (const [key, value] of Object.entries(obj)) {
+    fieldsArr.forEach(name => {
+      if (name === key) {
+        result[key] = value;
+      }
+    });
+  }
+  return result;
 };
