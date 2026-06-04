375eb02347d5582c113387096099cbcb69a70b8f
eugenyly <eugenypop@gmail.com>
2026-06-04 18:55:17 +0200

    +upd: createGetter;

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  if (!path) {
+    return;
+  }
 
+  return (obj) => {
+    const keys = path.split('.');
+    let result = obj;
+    for (const key of keys) {
+      if (result == null || !Object.hasOwn(result, key)) {
+        return undefined;
+      }
+
+      result = result[key];
+    }
+    return result;
+  };
 }
